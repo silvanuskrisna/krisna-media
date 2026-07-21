862fadac8bfd5a1f579cb1c923a42734ee492929
tokoberkat <berkatbersaudarabanjarmasin@gmail.com>
2026-07-21 12:31:11 +0800

    cleanup: remove temp scripts

--- tools/_write_key.js
+++
@@ -1,5 +0,0 @@
-require('fs').writeFileSync('D:\\tmp\\_supa_key.txt', 
-  require('fs').readFileSync('D:\\projects\\krisna-media\\.env.local','utf8')
-    .match(/SUPABASE_SERVICE_ROLE_KEY=(.+)/)[1].trim()
-)
-console.log('done')
